99162b1db4c4f9a49eb13f0439e5bde000173d9b
ShrutiMehta <shrutimehta973@gmail.com>
2021-02-03 21:22:00 +0530

    Updates

---
+++ precache-manifest.b6c82e2f3970ac0c0b9e33aa1a35aa5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9ce109b872a41ab9ecef9e1d5e1a5c",
+    "url": "/robot-friendss/index.html"
+  },
+  {
+    "revision": "59d84e16779069b26657",
+    "url": "/robot-friendss/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a98bb79fe62f8617fb5c",
+    "url": "/robot-friendss/static/css/main.86de20d1.chunk.css"
+  },
+  {
+    "revision": "59d84e16779069b26657",
+    "url": "/robot-friendss/static/js/2.a98e99f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-friendss/static/js/2.a98e99f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a98bb79fe62f8617fb5c",
+    "url": "/robot-friendss/static/js/main.3b55118e.chunk.js"
+  },
+  {
+    "revision": "fbf99f60d7793d218b3b",
+    "url": "/robot-friendss/static/js/runtime-main.541686dd.js"
+  }
+]);
